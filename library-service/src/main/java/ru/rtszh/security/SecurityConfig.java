8ccb2b754416610b7600301c8fe579df97091dfa
rtszh <arthur.sazhnev@yandex.ru>
2023-05-06 12:51:02 +0300

    update security config

--- library-service/src/main/java/ru/rtszh/security/SecurityConfig.java
+++ library-service/src/main/java/ru/rtszh/security/SecurityConfig.java
@@ -12,87 +12,26 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @EnableWebSecurity
-//@ComponentScan(basePackages = "ru.rtszh.service")
 public class SecurityConfig {
 
-    //    private static final String ADMIN_ROLE = "ADMIN";
-//    private static final String USER_ROLE = "USER";
-    private static final String ADMIN_ROLE = "ROLE_ADMIN";
-    private static final String USER_ROLE = "ROLE_USER";
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .antMatchers(HttpMethod.POST, "/api/v1/books").hasRole(ADMIN_ROLE)
-//                        .antMatchers(HttpMethod.PUT, "/api/v1/books/*").hasRole(ADMIN_ROLE)
-//                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/*").hasRole(ADMIN_ROLE)
-//                        .antMatchers("/").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers("/books/**").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.POST, "/api/v1/books/{id}/comments").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.PUT, "/api/v1/books/{id}/comments/*").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/{id}/comments/*").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers("/actuator/**").hasRole(ADMIN_ROLE)
-//                        .anyRequest().denyAll()
-//                )
-//                .formLogin();
-//        return http.build();
-//    }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()));
-//
-//        return http.build();
-//    }
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests((authorize) -> authorize
-//                        .antMatchers(HttpMethod.POST, "/api/v1/books").hasRole(ADMIN_ROLE)
-//                        .antMatchers(HttpMethod.PUT, "/api/v1/books/*").hasRole(ADMIN_ROLE)
-//                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/*").hasRole(ADMIN_ROLE)
-//                        .antMatchers("/").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers("/books/**").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.POST, "/api/v1/books/{id}/comments").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.PUT, "/api/v1/books/{id}/comments/*").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/{id}/comments/*").hasAnyRole(ADMIN_ROLE, USER_ROLE)
-//                        .antMatchers("/actuator/**").hasRole(ADMIN_ROLE)
-//                )
-//                .oauth2ResourceServer()
-//                .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()));
-//
-//        return http.build();
-//    }
+    private static final String ADMIN_AUTHORITY = "ROLE_ADMIN";
+    private static final String USER_AUTHORITY = "ROLE_USER";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests((authorize) -> authorize
-                        .antMatchers(HttpMethod.POST, "/api/v1/books").hasAuthority(ADMIN_ROLE)
-                        .antMatchers(HttpMethod.PUT, "/api/v1/books/*").hasAuthority(ADMIN_ROLE)
-                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/*").hasAuthority(ADMIN_ROLE)
-                        .antMatchers("/").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers("/books/**").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers(HttpMethod.POST, "/api/v1/books/{id}/comments").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers(HttpMethod.PUT, "/api/v1/books/{id}/comments/*").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/{id}/comments/*").hasAnyAuthority(ADMIN_ROLE, USER_ROLE)
-                        .antMatchers("/actuator/**").hasAuthority(ADMIN_ROLE)
+                        .antMatchers(HttpMethod.POST, "/api/v1/books").hasAuthority(ADMIN_AUTHORITY)
+                        .antMatchers(HttpMethod.PUT, "/api/v1/books/*").hasAuthority(ADMIN_AUTHORITY)
+                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/*").hasAuthority(ADMIN_AUTHORITY)
+                        .antMatchers("/").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers("/books/**").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers(HttpMethod.POST, "/api/v1/books/{id}/comments").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers(HttpMethod.PUT, "/api/v1/books/{id}/comments/*").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers(HttpMethod.DELETE, "/api/v1/books/{id}/comments/*").hasAnyAuthority(ADMIN_AUTHORITY, USER_AUTHORITY)
+                        .antMatchers("/actuator/**").hasAuthority(ADMIN_AUTHORITY)
                 )
                 .oauth2ResourceServer()
                 .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()));
