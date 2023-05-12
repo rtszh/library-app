73f8b4386dd7e26259b299d25975d016d67d53c6
rtszh <arthur.sazhnev@yandex.ru>
2023-05-12 19:05:41 +0300

    add user url-flow

--- library-service/src/main/java/ru/rtszh/repository/UserRepository.java
+++ library-service/src/main/java/ru/rtszh/repository/UserRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import ru.rtszh.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
-    @Query("{username:'?0'}")
-    Optional<User> findUserByUsername(String username);
+    List<User> findByUsernameAndBookId(String username, String bookId);
 }
